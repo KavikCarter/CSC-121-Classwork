475f5120bf6736aa850704336c4224d15005f261
KCarter <kavikmanone444@gmail.com>
2026-02-02 17:43:09 -0500

    Add files via upload

---
+++ python folder/Chapter 2/2.8 Number Eight.py
@@ -0,0 +1,9 @@
+# This program prints the number eight using different arithmetic operations
+#Addition: 5 + 3 = 8
+print(5 + 3)
+#Subtraction: 10 - 2 = 8
+print(10 - 2)
+#Multiplication: 4 * 2 = 8
+print(4 * 2)
+#Division: 16 / 2 = 8
+print(16 / 2)
