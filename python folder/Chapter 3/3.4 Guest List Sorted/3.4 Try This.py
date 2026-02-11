c0c50ad8ccb42c709ec84181239b19c7078c79ce
KCarter <kavikmanone444@gmail.com>
2026-02-10 20:32:10 -0500

    Add files via upload

---
+++ python folder/Chapter 3/3.4 Guest List Sorted/3.4 Try This.py
@@ -0,0 +1,7 @@
+# Dinner guest list (living or deceased)
+
+guests = ["Nikola Tesla", "Jimi Hendrix", "Steve Jobs"]
+
+# Print an invitation message for each guest
+for person in guests:
+    print(f"Hello {person}, I’d like to invite you to dinner with me. It would be an honor to have you as a guest!")
