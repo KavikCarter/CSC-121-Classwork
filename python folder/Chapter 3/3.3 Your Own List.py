0f928364307844eeb097076ff5b018380c1c524f
KCarter <kavikmanone444@gmail.com>
2026-02-10 20:55:33 -0500

    Add files via upload

---
+++ python folder/Chapter 3/3.3 Your Own List.py
@@ -0,0 +1,10 @@
+# Favorite mode of transportation: motorcycles
+
+motorcycles = ["Honda", "Yamaha", "Kawasaki", "Ducati", "Harley-Davidson"]
+
+# Print a series of statements using the list
+print(f"I would like to own a {motorcycles[0]} motorcycle.")
+print(f"I would like to own a {motorcycles[1]} motorcycle.")
+print(f"I would like to own a {motorcycles[2]} motorcycle.")
+print(f"I would like to own a {motorcycles[3]} motorcycle.")
+print(f"I would like to own a {motorcycles[4]} motorcycle.")
