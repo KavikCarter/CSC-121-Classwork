b8b91d1c9eb9c30e163b0b83dcbdefbf7b77a167
KCarter <kavikmanone444@gmail.com>
2026-02-24 14:32:57 -0500

    Add files via upload

---
+++ python folder/Chapter 5/5-5. AlienColors1-3.py
@@ -0,0 +1,15 @@
+# 5-5. Alien Colors Revisions
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points for shooting the alien!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points for shooting the alien!")
+else:
+    print("You just earned 15 points for shooting the alien!")
+
+   
+
+
+
